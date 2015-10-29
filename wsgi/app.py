65fcda0d871be8b22a3ac7c292eab961a38ce9cb
Vitthal Mirji <vitthal10@yahoo.in>
2015-10-29 12:35:50 +0530

    Update app.py

--- wsgi/app.py
+++ wsgi/app.py
@@ -1,5 +1,10 @@
 from flask import Flask , jsonify, render_template
 from textblob import TextBlob
+from subprocess import Popen, PIPE
+
+p = Popen('curl https://raw.github.com/sloria/TextBlob/master/download_corpora.py | python', shell=True,
+          stdout=PIPE, stderr=PIPE)
+out, err = p.communicate()
 
 app = Flask(__name__)
 
