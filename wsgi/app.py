1c059b05bd053c419470d4b2a47bf3e92f71fed5
Vitthal Mirji <vitthal10@yahoo.in>
2015-10-31 01:11:00 +0530

    Update app.py

--- wsgi/app.py
+++ wsgi/app.py
@@ -17,8 +17,8 @@ def sentiment(message):
 @app.route('/api/v1/pos/<message>')
 def pos(message):
 	text = TextBlob(message)
-	x = jsonify(text.noun_phrases)
-	response = {'pos' : text.tags, 'nou' : x}
+	//x = jsonify(text.noun_phrases)
+	response = {'pos' : text.tags}
 	return jsonify(response)
 
 @app.route('/api/v1/lem/<message>')
